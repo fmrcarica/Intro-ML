6c708b9ac147fc98e09fd0bc09660d4c84e6f93a
Theodor Otupas <otschmidt@ethz.ch>
2023-03-10 06:52:01 +0100

    new structure

--- Projects/Own solutions/P0/main.py
+++ Projects/Own solutions/P0/main.py
@@ -2,7 +2,15 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 
+
+### Read data
+
 df_train = pd.read_csv('/home/otps3141/Documents/Dokumente/ETH QE/Semester 2/Intro ML/Projects/P0/task0_sl19d1/train.csv')
+df_sample = pd.read_csv('/home/otps3141/Documents/Dokumente/ETH QE/Semester 2/Intro ML/Projects/P0/task0_sl19d1/sample.csv')
+df_test = pd.read_csv('/home/otps3141/Documents/Dokumente/ETH QE/Semester 2/Intro ML/Projects/P0/task0_sl19d1/test.csv')
+
+
+
 
 ### exclude first column
 # print(df_train.iloc[:,1:])
@@ -28,17 +36,6 @@ w_star = np.linalg.inv(X.T@X)@(X.T@y)
 # print(w_star)
 
 ### Gradient descent approach
-
-w = np.ones((X.shape[1], 1))
-
-# y_array = y.to_numpy()
-# array = X.dot(w).to_numpy()
-
-error = y - X.dot(w)
-grad = -(X.T).dot(error)[0]
-# print(grad)
-
-
 def grad_desc(X, y, rate = 0.01, iterations = 100):
     w = np.ones((X.shape[1], 1))
     for i in range(iterations):
@@ -47,28 +44,20 @@ def grad_desc(X, y, rate = 0.01, iterations = 100):
         w = w - rate*grad
     return w
 
-
-
 # print(grad_desc(X, y))
 
 
 
-df_test = pd.read_csv('/home/otps3141/Documents/Dokumente/ETH QE/Semester 2/Intro ML/Projects/P0/task0_sl19d1/test.csv')
+
+### Prediction for test data set
 
 X_test = df_test.iloc[:,1:]
 
-print(X_test)
+y_predict = X_test.dot(w_star)
 
-y_outcome = X_test.dot(w_star)
+df_sample['y'] = y_predict
 
-
-df_sample = pd.read_csv('/home/otps3141/Documents/Dokumente/ETH QE/Semester 2/Intro ML/Projects/P0/task0_sl19d1/sample.csv')
-
-df_sample['y'] = y_outcome
-
-print(df_sample)
-
-df_sample.round(3).to_csv('Projects/P0/task0_sl19d1/sample.csv', index=False)
+df_sample.round(3).to_csv('Projects/Own solutions/P0/sample.csv', index=False)
 
 
 
